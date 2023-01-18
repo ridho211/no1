d1f7834a1d2329c27fd729fb569405e0a6964686
ridho211 <84274473+ridho211@users.noreply.github.com>
2023-01-18 23:17:54 +0700

    Add files via upload

---
+++ no1/halaman/page1.php
@@ -0,0 +1,14 @@
+<center><table border="1px" width="70%" cellpadding="5" cellspacing="0">
+	<tr>
+		<td>NAMA       : RIDHO IKHRAM</td>
+	</tr>
+	<tr><td>JABATAN    : DIREKTUR UTAMA</td></tr>
+		<tr><td>ALAMAT : BENGKULU</td></tr>
+		<tr><td>E-MAIL : ridhoikhram180@gmail.com</td></tr>
+		<tr><td>NO HP : 081367600167</td></tr>
+	<tr>
+		<td style="background-color:silver"><img src="img/aa.JPG" width="400px" height="400px"></td></tr>
+
+		
+</table>
+</center>
---
+++ no1/halaman/page4.php
@@ -0,0 +1,2 @@
+<h1 style="color: white">PETA ANAK PT ELANG PERMATA</h1>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3977.1026938808636!2d103.12621431412272!3d-4.575571649346142!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e37bf7adc19f1cb%3A0xe5c50d72a72079b7!2sPT%20Dinamika%20Selaras%20Jaya!5e0!3m2!1sid!2sid!4v1673631835231!5m2!1sid!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
